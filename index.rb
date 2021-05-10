cd6c0f31bc805f17c595bc1ef7983f2893cfed0d
Alex baulderstone <alex.baulderstone@coderacademy.edu.au>
2021-05-10 11:29:39 +1000

    creates methods.rb and refactors group add and random order output method

--- index.rb
+++ index.rb
@@ -1,33 +1,24 @@
-group = ["alice"]
+require_relative "methods"
+group = ["alice", "bob", "charlie"]
+
+
+
 
 while true 
-    puts "There are #{group.length == 1 ? '1 member':  group.length.to_s + 'members'} in the group"
-    puts "Press 1) to add a member to the group"
-    puts "Press 2) display a random group"
-    puts "Press 3) to select random user"
-    puts "Press 4) to quit"
-    #take input from user
-    menu_input = gets.chomp.to_i
+    #TODO: Fix grammar
+    group_length = group.length
+    output_group_size(group_length)
 
-    case menu_input
+    case menu_input_select
     when 1
-        puts "Enter name:"
-        name = gets.chomp
-        group.push(name)
+     add_member_to_group(group)
     when 2 
         puts "Random group output:"
         random_order_loop_running = true
         while random_order_loop_running
-            # copied_group = group.clone 
-            # while copied_group.length > 0
-            #     random_index = rand(copied_group.length - 1)
-            #     puts copied_group[random_index].capitalize
-            #     copied_group.delete_at(random_index)
-            # end 
+          
             
-            group.shuffle.each do |name| 
-                puts name.capitalize
-            end 
+            output_random_group_order(group)
             
             puts "press 1 to go back"
             puts "press 2 to quit"
---
+++ methods.rb
@@ -0,0 +1,43 @@
+# puts "Loaded up the methods file"
+
+def output_group_size(number)
+    if number == 1
+        puts "There is one member in the group"
+    elsif number == 0 
+        puts "The group is empty"
+    else
+        puts "There are #{number} members in the group"
+    end  
+end 
+
+def menu_input_select 
+    puts "Press 1) to add a member to the group"
+    puts "Press 2) display a random group"
+    puts "Press 3) to select random user"
+    puts "Press 4) to quit"
+    #take input from user
+    menu_input = gets.chomp.to_i
+    return menu_input
+end
+
+def output_random_group_order(collection)
+    collection.shuffle.each do |name| 
+        puts capitalize_multi_word_string(name)
+    end 
+end 
+
+
+def add_member_to_group(arr)
+    puts "Enter name:"
+    name = gets.chomp
+    arr.push(name)
+end
+
+def capitalize_multi_word_string(str)
+    arr = str.split(" ")
+    capitalised_array = arr.map {|word| word.capitalize}
+    capitalised_string = capitalised_array.join(" ")
+    return capitalised_string
+end 
+
+output_random_group_order(["tony hawk", "the rock", "someone cool"])
