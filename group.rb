76fdbf0ee9a784a16d2a2ae31c84546e43670f2d
Alex baulderstone <alex.baulderstone@coderacademy.edu.au>
2021-05-11 12:40:35 +1000

    sets up path and file system

--- group.rb
+++ group.rb
@@ -1,9 +1,10 @@
 class Group
     attr_reader :name, :names_array
     
-    def initialize(group_name)
+    def initialize(group_name, file_path)
         @name = group_name
-        @names_array = ["Alice", "Bob", "Charlie", "Dylan", "Eddie"]
+        @path = file_path
+        @names_array = self.path_to_array
     end
 
     def randomise_order 
@@ -20,5 +21,27 @@ class Group
         @names_array.push(name)
     end 
 
+    def path_to_array
+        begin 
+            array = File.readlines(get_file_path).map {|name| name.strip}
+        rescue 
+            puts "Invalid path, creating file"
+            File.open(get_file_path, "w") do |file| 
+                file.write("")
+            end 
+            array = []
+        end 
+        return array 
+    end 
+
+    private 
+    def get_file_path
+        @path
+    end 
 
 end 
+
+test_group = Group.new("Test Group", './groups/test-group.txt')
+test_group.output_random_array
+
+puts "after error"
