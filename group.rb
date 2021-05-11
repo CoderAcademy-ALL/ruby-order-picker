1a7e110a2ce89f6ae2a42a88a66bff8120b83282
Alex baulderstone <alex.baulderstone@coderacademy.edu.au>
2021-05-11 12:13:01 +1000

    writes tests for add_name, randomise_order etc

--- group.rb
+++ group.rb
@@ -3,8 +3,22 @@ class Group
     
     def initialize(group_name)
         @name = group_name
-        @names_array = []
+        @names_array = ["Alice", "Bob", "Charlie", "Dylan", "Eddie"]
     end
+
+    def randomise_order 
+        @names_array.shuffle
+    end
+
+    def output_random_array
+        randomise_order.each do |name|
+            puts name 
+        end 
+    end
+
+    def add_name(name)
+        @names_array.push(name)
+    end 
 
 
 end 
